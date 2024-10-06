47865f669423e12b31b3701e375cc246cc814412
HelloWorldForGao <lich421186@126.com>
2024-10-06 14:31:56 +0800

    更华丽的界面

---
+++ 1.js
@@ -0,0 +1,6 @@
+function watt_toolkit()
+{
+    document.getElementById("watt_toolkit").innerHTML = "没啥要说的了，选啥都一样";
+    window.alert("下载Watt Toolkit，记得点开Github加速哦")
+    console.log("已前往Watt toolkit")
+}
